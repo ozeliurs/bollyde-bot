afbe6bd7a3ca42edbff7d48cd47c28ef6348e47d
Ozeliurs <ozeliurs@gmail.com>
2022-03-17 18:59:57 +0100

    Added Pinel Spamming

--- www/main.py
+++ www/main.py
@@ -1,6 +1,6 @@
 import discord
 
-with open(".discord", "r", encoding="utf8") as secret:
+with open(f"{__file__.split('/main.py')[0]}/.discord", "r", encoding="utf8") as secret:
     TOKEN = secret.read()
 
 client = discord.Client()
@@ -8,7 +8,23 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        print(
+            f'{client.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})\n'
+        )
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if str(message.author) == "Maxime BILLY#9875":
+        pass
+
+    if str(message.author) == "antøskins#8106":
+        await message.channel.send("https://cdn.discordapp.com/attachments/755446976631406725/953788321253949510/unknown.png")
 
 
 def main():
