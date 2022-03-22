680eff5b517fc524a7f8abb7664832c87da0d357
Ozeliurs <ozeliurs@gmail.com>
2022-03-22 11:25:21 +0100

    sutom score

--- www/main.py
+++ www/main.py
@@ -20,6 +20,25 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    if message.content == "$sutom":
+        with open("/data/sutom.csv", "r", encoding="utf8") as sutom:
+            data = [x.split(",").strip() for x in sutom.read().split("\n")]
+
+        players = {}
+
+        for play in data:
+            if play[1] not in players:
+                players[play[1]] == 0
+            if int(players[play[2]]) - 5 > 0:
+                players[play[1]] += int(players[play[2]]) - 5
+
+        out = "Résultats :\n"
+
+        for player in players:
+            out += f"{player} - {players[player]} points\n"
+
+        message.channel.send(out)
+
     if message.content.startswith("SUTOM #"):
         meta = message.content.split("\n")[0]
         _, day, score = meta.split(" ")
