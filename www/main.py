d4bc47d705cb5076283dbf5d93811cfa1d40c809
Ozeliurs <ozeliurs@gmail.com>
2022-03-22 14:01:44 +0100

    debug sutom

--- www/main.py
+++ www/main.py
@@ -30,7 +30,11 @@ async def on_message(message):
     if message.content == "$sutom":
         with open("/data/sutom.csv", "r", encoding="utf8") as sutom:
             print(sutom.read())
-            data = [x.split(",") for x in sutom.read().split("\n") if x != ""]
+            sutom = sutom.read().split("\n")
+            data = []
+            for su in sutom:
+                if su != "":
+                    data.append(su.split(","))
 
         print(data)
 
