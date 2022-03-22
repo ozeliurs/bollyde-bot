69660a19bd5b144ddb9b89be2000ef9367296a2f
BoLLYde Maxime <ozeliurs@gmail.com>
2022-03-22 10:49:05 +0100

    Update main.py

--- www/main.py
+++ www/main.py
@@ -20,12 +20,6 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if str(message.author) == "Maxime BILLY#9875":
-        pass
-
-    if str(message.author) == "antøskins#8106":
-        await message.channel.send("https://cdn.discordapp.com/attachments/755446976631406725/953788321253949510/unknown.png")
-
 
 def main():
     client.run(TOKEN)
