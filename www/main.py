e50f8d0e25c92360bad4e1fb37567abcc023b785
Ozeliurs <ozeliurs@gmail.com>
2022-03-22 13:39:10 +0100

    sutom bugfix + id

--- www/main.py
+++ www/main.py
@@ -6,6 +6,14 @@ with open(f"{__file__.split('/main.py')[0]}/.discord", "r", encoding="utf8") as 
 client = discord.Client()
 
 
+def buildIdTable(csv):
+    dic = {}
+    for line in csv:
+        if line[1] not in dic:
+            dic[line[1]] = line[2]
+    return dic
+
+
 @client.event
 async def on_ready():
     for guild in client.guilds:
@@ -19,34 +27,35 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
+    sutom = "74,325623032456413186,test,3\n74,325623032456413186,test,3"
     if message.content == "$sutom":
         with open("/data/sutom.csv", "r", encoding="utf8") as sutom:
             data = [x.split(",") for x in sutom.read().split("\n") if x != ""][:-1]
+
+        table = buildIdTable(data)
 
         players = {}
 
         for play in data:
             if play[1] not in players:
                 players[play[1]] = 0
-            if 7 - int(play[2]) > 0:
-                players[play[1]] += 7 - int(play[2])
-
-        await message.channel.send(str(players))
+            if 7 - int(play[3]) > 0:
+                players[play[1]] += 7 - int(play[3])
 
         out = "Résultats :\n"
 
         for player in players:
-            out += f"{client.get_user(int(player)).name} - {players[player]} points\n"
+            out += f"{table[player]} - {players[player]} points\n"
 
         await message.channel.send(out)
+
 
     if message.content.startswith("SUTOM #"):
         meta = message.content.split("\n")[0]
         _, day, score = meta.split(" ")
 
         with open("/data/sutom.csv", "a", encoding="utf8") as sutom:
-            sutom.write(f"\n{day.strip('#')},{message.author.id},{score.split('/')[0]}")
+            sutom.write(f"\n{day.strip('#')},{message.author.id},{message.author},{score.split('/')[0]}")
 
 
 def main():
